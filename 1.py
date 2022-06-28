4d436694903d853494e242af6ca1904ffde71830
Dickson666 <1399031914@qq.com>
2022-06-28 21:51:45 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,9 @@
-import requests;
-import random
-import string
+import requests
 
 for i in range(10):
     a = requests.get('https://api.ixiaowai.cn/api/api.php');
     print(a);
     print(a.status_code);
-    with open("photos\\" + ''.join(random.sample(string.ascii_letters + string.digits, 20)) + ".jpg", "wb+") as f:
+    with open("photos/" + str(i) +".jpg", "wb+") as f:
        f.write(a.content);
     print("Finish!");
